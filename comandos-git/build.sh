2fdabd33682753bd995bd86061b34034c65c0965
matheusDall <matheus.dalmasomariano@gmail.com>
2025-03-26 19:38:47 -0300

    organizando 5

---
+++ comandos-git/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# " ./NomeDoArquivo "  -> para rodar 
+
+
+docker build -t git-container .
+
+docker run -it git-container
+
+git init 
+
+git config --global user.name "matheusDall"
+
+git config --global user.email "matheus.dalmasomariano@gmail.com"
+
+git clone https://github.com/matheusDall/Repositorio-para-o-TCC
+
+ls
+
+git status
+
+cd Repositorio-para-o-TCC
+
+mkdir testandDenovo
+
+cd testandDenovo
+
+touch miau.txt
+
+ls
+
+git add .
+
+git commit -m "commit do teste"
+
+git branch
+
+push orgin main
+
+
+
+token github == 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
