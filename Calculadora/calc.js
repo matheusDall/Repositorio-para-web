ec719e674d538f7fbcce635ba8ed72992feefd0c
matheusDall <matheus.dalmasomariano@gmail.com>
2025-07-02 22:18:40 -0300

    fazendo a calculadora

--- Calculadora/calc.js
+++ Calculadora/calc.js
@@ -1,2 +1,18 @@
 // funçao "eval()" transforma String em equaçao matematica ---usar
 
+let formula = "";
+
+function calcular(elemento){
+
+    if(elemento != "enter"){
+    
+        formula += elemento
+    }
+
+    if(elemento == "enter"){
+
+        console.log(eval(formula));
+
+    
+    }
+}
